60d217bc865992c158c9f8e6bd9988df34f6fe9c
rahulraparia-ac <rahul.raparia@acuverconsulting.com>
2022-03-23 02:56:03 +0530

    biFunctions

--- src/main/java/functionalInterface/_Function.java
+++ src/main/java/functionalInterface/_Function.java
@@ -1,5 +1,6 @@
 package functionalInterface;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _Function {
@@ -11,12 +12,18 @@ public class _Function {
         int multiplyBy10 = multiplyBy10Function.apply(increment);
         System.out.println(multiplyBy10);
 
-        Function<Integer, Integer> addBy1andThenMultiplyBy10 = incrementByOneFunction.andThen(multiplyBy10Function);
-        System.out.println(addBy1andThenMultiplyBy10.apply(1));
+        //BiFunction takes two arguments and produces one result
+        System.out.println(incrementByOneAndMultiplyFunction.apply(4,500));
 
     }
     static Function<Integer, Integer> incrementByOneFunction = number -> number + 1;
     static Function<Integer, Integer> multiplyBy10Function = number -> number*10;
+    static BiFunction<Integer, Integer, Integer> incrementByOneAndMultiplyFunction =
+            (numberToIncrementByOne, numberToMultiplyBy)
+                    -> (numberToIncrementByOne + 1) * numberToMultiplyBy;
+    static int incrementByOneAndMultiply(int number, int numToMultiplyBy) {
+        return (number +1) * numToMultiplyBy;
+    }
 
 
 }

4875b8a917c64204449034fc7f1aab024e6be259
rahulraparia-ac <rahul.raparia@acuverconsulting.com>
2022-03-25 17:14:24 +0530

    Suppliers

---
+++ src/main/java/functionalInterface/_Supplier.java
@@ -0,0 +1,18 @@
+package functionalInterface;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+public class _Supplier {
+    public static void main(String[] args) {
+        System.out.println(getDBConnectionUrl());
+
+        System.out.println("List of DB Connection URL");
+        System.out.println(getDBConnectionUrlSupplier.get());
+    }
+    static String getDBConnectionUrl () {
+        return "jdbc://localhost:5432/users";
+    }
+
+    static Supplier<List<String>> getDBConnectionUrlSupplier = () -> List.of("jdbc://localhost:5432/users");
+}
